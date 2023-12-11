834dc7ea5104f990ba467b21b435d2840a3a9453
olaf <juliendupdupdup@gmail.com>
2023-12-11 17:26:18 +0100

    Routage OK

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,46 @@
-function App() {
-  return (
-    
-      "Chez vous, partout et ailleurs"
-    
-  )
+import { RouterProvider, Outlet, createBrowserRouter } from "react-router-dom";
+import Rooms from "./pages/Rooms";
+import ErrorPage from "./pages/ErrorPage";
+import About from "./pages/About";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+
+
+const router = createBrowserRouter([
+  {
+    path : '/',
+    element: <Root/>,
+    errorElement: <ErrorPage/>,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: 'rooms/:id',
+            element: <Rooms/>
+          },
+      {
+        path: 'about',
+        element: <About/>
+      },
+    ]
+  }
+])
+
+function Root () {
+  return <>
+    <Header/>
+    <main className="main-container">
+      <Outlet/>
+    </main>
+    <Footer/>
+  </>
 }
 
-export default App
+function App() {
+  return <RouterProvider router={router}/>
+}
+
+export default App;
