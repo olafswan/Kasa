bf1a91fbfbe7b1158a4b59e677876b06bde63629
olaf <juliendupdupdup@gmail.com>
2023-12-11 17:29:54 +0100

    1st commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import TopSection from "../components/TopSection";
+import RoomsGallery from "../components/RoomsGallery";
+
+
+export default function Home() {
+
+  return (
+    <>
+    
+       
+        <TopSection/>
+
+        <RoomsGallery/>
+
+        
+
+
+    </>
+
+  );
+}
