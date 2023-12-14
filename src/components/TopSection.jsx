d48d07cecc48ee8410b691349bdfc278866043fb
olaf <juliendupdupdup@gmail.com>
2023-12-14 16:46:04 +0100

    img and text as props

--- src/components/TopSection.jsx
+++ src/components/TopSection.jsx
@@ -1,12 +1,15 @@
-export default function TopSection() {
+export default function TopSection({img, text}) {
 
 // const style1 = {maxHeight: '100px', borderRadius: '15px', border: "10px solid red"}
 
 // const styleA = {height: "100px", width: "500px", borderRadius: '15px', borderBottom: '10px dotted red', display: "none"}
 
   return (
-    <figure>
-          <img src="./src/assets/shore.jpg" alt="Photo de bord de mer escarpé" />
-    </figure>
+    <div className="top-section">
+          <img src={img} />
+          {/* <img src="./src/assets/shore.jpg" alt="Photo de bord de mer escarpé" /> */}
+    <p>{text}</p>
+    {/* <p>Chez vous, <br/>partout et ailleurs</p> */}
+    </div>
   );
 }
