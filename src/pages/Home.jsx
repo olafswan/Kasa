0165a38751485c92a5ff1ad37aa74afc03bc068c
olaf <juliendupdupdup@gmail.com>
2023-12-14 16:48:05 +0100

    img and text added top the top-section component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,13 @@ import TopSection from "../components/TopSection";
 import RoomsGallery from "../components/RoomsGallery";
 
 
+
 export default function Home() {
 
   return (
     <>
     
-        <TopSection/>
+        <TopSection img="./src/assets/shore.jpg" text={["Chez vous, ", <br/>,"partout et ailleurs"]}/>
 
         <RoomsGallery/>
 
