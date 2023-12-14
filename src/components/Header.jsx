788fd73e109c5e1770defbec59c44782c14b642f
olaf <juliendupdupdup@gmail.com>
2023-12-14 16:43:30 +0100

    typo + inline style deleted

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,17 +4,19 @@ import { NavLink } from "react-router-dom";
 
 export default function Header() {
 
-  const style = {display: 'flex', flexDirection: 'row', borderBottom: '1px dotted grey'}
-
   return (
     <header>
-      <img src="./src/assets/kasa.svg" alt="Logo Kasa" className="lmj-logo" />
-      <nav style = {style}>
+      <NavLink to="/" className="header__logo-container">
+        <img src="./src/assets/kasa.svg" alt="Logo Kasa" />
+      </NavLink>
+      <nav className="header__menu">
 
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/about">A propos</NavLink>
+        <NavLink to="/">Accueil</NavLink>
+        <NavLink to="/about">A Propos</NavLink>
+        
+        {/* TODO underline home or about when this is the current page */}
   
-        {/* {rooms.map((room) => (<NavLink to={`/rooms/${room}`} style = {style} key={room}>Appartement {room}</NavLink>))} */}
+        {/* {rooms.map((room) => (<NavLink to={`/rooms/${room}`} key={room}>Appartement {room}</NavLink>))} */}
 
       </nav>
     </header>
