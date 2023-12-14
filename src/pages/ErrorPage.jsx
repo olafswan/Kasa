7e74be24a4f1dd3f79b9676448fd37c3a96774b5
olaf <juliendupdupdup@gmail.com>
2023-12-14 16:46:41 +0100

    style + header and footer

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,16 +1,25 @@
 import { useRouteError } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 function PageError () {
     const error = useRouteError()
     console.log(error);
     return <>
-        <h1>Une erreur est survenue</h1>
-        <p>
+        <Header/>
+        <div className="error-page">
+        <h1>Oops!</h1>
+        <h2>La page est introuvable !</h2>
+        <NavLink to="/">Revenir à l&#39;accueil</NavLink>
+        </div>
+        {/* <p>
             {error?.error?.toString() ?? error?.toString()}
         </p>
         <p>
         <i>{error.statusText || error.message}</i>
-        </p>
+        </p> */}
+        <Footer/>
     </>
 }
 
