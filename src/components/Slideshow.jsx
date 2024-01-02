f04e2764e7c098e0e57b4d7eabfa78fb27ceeb52
olaf <juliendupdupdup@gmail.com>
2024-01-02 19:10:58 +0100

    ajout de commentaires

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -8,20 +8,19 @@ const Slideshow = (props) => {
   
   const hasSeveralPics = picturesData.length > 1;
 
-
-  // const slider = (pictures, direction) => {
-  //   // récuperer l'index de la pic
-
-  //   // selon le sens et l'index de la pic afficher la pic en question
-  // };
-
+  // fonction précédent
   const goToPrevious = () => {
+    // vérifie si l'image acctuelle est la 1ère
     const isFirstPic = currentIndex === 0;
+    // si l'image était la 1ère afficher la dernière sinon la précédente
     const newIndex = isFirstPic ? picturesData.length - 1 : currentIndex - 1;
     setCurrentIndex(newIndex)
   }
+  // fonction suivant
   const goToNext = () => {
+    // vérifie si l'image acctuelle est la dernière
     const isLastPic = currentIndex === picturesData.length - 1;
+    // si l'image était la dernière afficher la 1ère sinon la suivante
     const newIndex = isLastPic ? 0 : currentIndex + 1;
     setCurrentIndex(newIndex)
   }
@@ -30,17 +29,17 @@ const Slideshow = (props) => {
 
   return (
     <div className="slideshow">
-        {/* <p style={{fontStyle: "italic"}}>Slideshow logement n°{props.data.id}</p> */}
-        {/* <img style={{width: "85vw", borderRadius: "25px", margin: "auto"}} src={props.data.pictures[0]}/> */}
-        <img className="room-picture" src={picturesData[currentIndex]}/>
-        <p className={`${!hasSeveralPics ? 'hide' : ''}`}>{currentIndex + 1}/{picturesData.length}</p>
-        {/* <img src="../src/assets/arrow-back.svg"/> */}
-        <div className={`slide back ${!hasSeveralPics ? 'hide' : ''}`} onClick={goToPrevious}>
+      <img className="room-picture" src={picturesData[currentIndex]} alt={props.data.title}/>
+      {/* numéro de l'image affiché uniquement si plusieurs images  */}
+      <p className={`${!hasSeveralPics ? 'hide' : ''}`}>{currentIndex + 1}/{picturesData.length}</p>
+      {/* bouton précédent affiché uniquement si plusieurs images  */}
+      <div className={`slide back ${!hasSeveralPics ? 'hide' : ''}`} onClick={goToPrevious}>
         <img className="arrow" src="../src/assets/arrow-back.svg"/>
-        </div>
-        <div className={`slide forward ${!hasSeveralPics ? 'hide' : ''}`} onClick={goToNext}>
+      </div>
+      {/* bouton suivant affiché uniquement si plusieurs images  */}
+      <div className={`slide forward ${!hasSeveralPics ? 'hide' : ''}`} onClick={goToNext}>
         <img className="arrow" src="../src/assets/arrow-forward.svg"/>
-        </div>
+      </div>
     </div>
   );
 }
