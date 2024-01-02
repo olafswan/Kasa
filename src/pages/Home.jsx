de5cdf9c17529d20b2fac6aee9f44b15131b49c8
olaf <juliendupdupdup@gmail.com>
2024-01-02 20:02:08 +0100

    texte de la TopSection dans un array sans BR

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,15 @@ import RoomsGallery from "../components/RoomsGallery";
 
 export default function Home() {
 
+  const topText = ["Chez vous, ", "partout et ailleurs"]
+
   return (
     <>
     
       {/* <TopSection img="./src/assets/shore.jpg" text={"Chez vous, partout et ailleurs"}/> */}
 
-      <TopSection img="./src/assets/shore.jpg" text={["Chez vous, ", <br/>,"partout et ailleurs"]}/>
+      {/* <TopSection img="./src/assets/shore.jpg" text={["Chez vous, ", <br/>,"partout et ailleurs"]}/> */}
+      <TopSection img="./src/assets/shore.jpg" text={topText}/>
 
       {/* <TopSection key="12345" img="./src/assets/shore.jpg" text={[`Chez vous, `, <br/>,`partout et ailleurs`]}/> */}
 
