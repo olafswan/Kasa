b0b21269757749a662719d4f09fd85063cd5343e
olaf <juliendupdupdup@gmail.com>
2024-01-02 18:46:22 +0100

    suppresion code commenté

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,5 @@
 import { NavLink } from "react-router-dom";
 
-// const rooms = [1, 2, 3, 4, 5]
-
 export default function Header() {
 
   return (
@@ -13,10 +11,6 @@ export default function Header() {
 
         <NavLink to="/">Accueil</NavLink>
         <NavLink to="/about">A Propos</NavLink>
-        
-        {/* TODO underline home or about when this is the current page */}
-  
-        {/* {rooms.map((room) => (<NavLink to={`/rooms/${room}`} key={room}>Appartement {room}</NavLink>))} */}
 
       </nav>
     </header>
