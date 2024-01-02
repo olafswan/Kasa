835fce6517a96fca9b1943272792be1bf23f684c
olaf <juliendupdupdup@gmail.com>
2024-01-02 18:28:26 +0100

    commentaires

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Home from "./pages/Home";
 import './styles/css/style.css'
 
 
+// rooting
 const router = createBrowserRouter([
   {
     path : '/',
@@ -30,6 +31,7 @@ const router = createBrowserRouter([
   }
 ])
 
+// layout
 function Root () {
   return <>
     <Header/>
