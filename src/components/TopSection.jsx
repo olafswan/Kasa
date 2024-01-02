d5ff4db73cc9e14716a644b80159330671b59ca9
olaf <juliendupdupdup@gmail.com>
2024-01-02 19:11:57 +0100

    suppresion code commenté

--- src/components/TopSection.jsx
+++ src/components/TopSection.jsx
@@ -3,9 +3,7 @@ export default function TopSection(props) {
   return (
     <div className="top-section">
           <img src={props.img} />
-          {/* <img src="./src/assets/shore.jpg" alt="Photo de bord de mer escarpé" /> */}
     <p>{props.text}</p>
-    {/* <p>Chez vous, <br/>partout et ailleurs</p> */}
     </div>
   );
 }
