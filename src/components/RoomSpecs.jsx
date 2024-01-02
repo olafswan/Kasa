1132d2b8529efa1363a183ba89beec5ef2e101db
olaf <juliendupdupdup@gmail.com>
2024-01-02 19:03:30 +0100

    ajout des commentaires

--- src/components/RoomSpecs.jsx
+++ src/components/RoomSpecs.jsx
@@ -2,14 +2,20 @@ import Collapse from "../components/Collapse";
 
 const RoomSpecs = (props) => {
 
+    // fonction pour converting la note en étoiles
     const staromator = (rating) => {
+        // vérifie si la note est entre 0 et 5
         if (Number(rating) >= 0 && Number(rating) <= 5) {
+            // étoiles remplies = la note
             const filledStar = Number(rating);
+            // étoiles vides = 5 - la note
             const emptyStar = 5 - Number(rating);
             let result =[];
+            // ajoute le nombre d'étoiles remplies
             for (let i = 0; i < filledStar; i++) {
                 result.push(<img src="../src/assets/star-filled.svg" key={`filledStar-${i}`} />)
             }
+           // ajoute le nombre d'étoiles vides
             for (let i = 0; i < emptyStar; i++) {
                 result.push(<img src="../src/assets/star-empty.svg" key={`emptyStar-${i}`} />)
             }
@@ -17,11 +23,16 @@ const RoomSpecs = (props) => {
         }
     }
 
+    // fonction pour séparer le nom du prénom
     const namesplitor = name => name.match(/\w+/g);
 
+    // fonction pour mettre en forme le lieu comme sur la maquette ("Ile de France - Paris 17e" => "Paris 17e, Ile de France")
     const locationstylor = (location) => {
+        // split au niveau du tiret
         const locationArray = location.split(" - ");
+        // ajoute la virgule et l'espace après la ville
         locationArray[1] += ", ";
+        // inverse le tableau
         return locationArray.reverse();
     };
     
@@ -31,23 +42,26 @@ const RoomSpecs = (props) => {
             <div className="left">
                 <h1>{props.data.title}</h1>
                 <h2>
+                    {/* affiche le lieu avec la bonne mise en forme */}
                     {locationstylor(props.data.location).map((word, index) => (<><span key={`${word}${index}`}>{word}</span></>))}
                 </h2>
                 <div className="tag-container">
+                    {/* map pour afficher les tags */}
                     {props.data.tags.map((tag) => (<p className="tag" key={tag}>{tag}</p>))}
                 </div>
             </div>
             <div className="right">
                 <div className="host">
                     <div className="profil-name">
+                        {/* affiche le nom et prénom sur des lignes différents */}
                         {namesplitor(props.data.host.name).map((word) => (<p key={`name-${word}`}>{word}</p>))}
                     </div>
-                    {/* <p>{props.data.host.name}</p> */}
                     <div className="profil-picture">
                         <img src={props.data.host.picture} alt={props.data.host.name}/>
                     </div>
                 </div>
                 <div className="rating">
+                    {/* affiche la note sous forme d'étoile  */}
                     {staromator(props.data.rating).map((star) => (star))}
                 </div>
             </div>
@@ -57,6 +71,7 @@ const RoomSpecs = (props) => {
                 <p>{props.data.description}</p>
             </Collapse>
             <Collapse label="Équipements" >
+                {/* map pour afficher la liste d'équipements  */}
                 {(props.data.equipments).map((equipment) => (<p key={equipment}>{equipment}</p>))}
             </Collapse>
         </div>
