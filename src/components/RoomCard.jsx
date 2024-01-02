13b97c16c0ad39e7b2577c01b11787f779c5cc29
olaf <juliendupdupdup@gmail.com>
2024-01-02 18:50:03 +0100

    ajout alt pour l'image

--- src/components/RoomCard.jsx
+++ src/components/RoomCard.jsx
@@ -5,7 +5,7 @@ export default function RoomCard({room}) {
     
         <NavLink to={`/rooms/${room.id}`} className='card'>
 
-            <img src={room.cover}></img>
+            <img src={room.cover} alt={room.title}></img>
             <h2>{room.title}</h2>
             
         </NavLink>
